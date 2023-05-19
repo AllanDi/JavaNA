7c3f39587749d336e44aa4a58d18b2341c8583c1
Allan <allandrs@gmail.com>
2023-05-19 15:48:09 -0300

    Atualização exercicios

--- src/ExerciciosBasicos/Parte1Exercicio1.java
+++ src/ExerciciosBasicos/Parte1Exercicio1.java
@@ -2,13 +2,14 @@ package ExerciciosBasicos;
 
 import java.util.Scanner;
 
-public class Parte1 {
+public class Parte1Exercicio1 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("EB parte 1");
-        System.out.println("Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa:\n");
+        System.out.println("Exercicio 1");
+        System.out.println("Faça um programa para ler dois valores inteiros, e depois mostrar na tela " +
+                "a soma desses números com uma mensagem explicativa:\n");
 
         int valor1;
         int valor2;
@@ -22,8 +23,5 @@ public class Parte1 {
         int somaValores = valor1 + valor2;
 
         System.out.println("A soma dos valores é:" + somaValores);
-
-
-
     }
 }
---
+++ src/ExerciciosBasicos/Parte1Exercicio2.java
@@ -0,0 +1,26 @@
+package ExerciciosBasicos;
+
+import java.util.Scanner;
+
+public class Parte1Exercicio2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Exercicio 2");
+        System.out.println("Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da" +
+                " área deste círculo com quatro casas decimais.\n" +
+                "Fórmula da área: area = π . raio2\n" +
+                "Considere o valor de π = 3.14159\n");
+
+        double pi = Math.PI;
+        double raio;
+
+        System.out.print("Digite o valor do raio do circulo para se calcular a area: ");
+        raio = sc.nextDouble();
+
+        double area = pi * raio * raio;
+
+        System.out.printf("A soma dos valores é: %.4f", area);
+    }
+}
