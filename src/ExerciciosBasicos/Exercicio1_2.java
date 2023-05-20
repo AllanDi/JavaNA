65d370137c4f0231a85f5d08d6882583d9511b5e
Allan <allandrs@gmail.com>
2023-05-19 23:08:25 -0300

    exercicio 3 adicionado

--- src/ExerciciosBasicos/Exercicio1_2.java
+++ src/ExerciciosBasicos/Exercicio1_2.java
@@ -8,8 +8,8 @@ public class Exercicio1_2 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Exercicio 2");
-        System.out.println("Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da" +
-                " área deste círculo com quatro casas decimais.\n" +
+        System.out.println("Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da " +
+                "área deste círculo com quatro casas decimais.\n" +
                 "Fórmula da área: area = π . raio2\n" +
                 "Considere o valor de π = 3.14159\n");
 
--- src/ExerciciosBasicos/Exercicio1_3.java
+++ src/ExerciciosBasicos/Exercicio1_3.java
@@ -1,4 +1,36 @@
 package ExerciciosBasicos;
 
+import java.util.Scanner;
+
 public class Exercicio1_3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Exercicio 3");
+        System.out.println("Fazer um programa para ler quatro valores inteiros A, B, C e D. " +
+                "A seguir, calcule e mostre a diferença do produto\n" +
+                "de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).");
+
+        int valor1;
+        int valor2;
+        int valor3;
+        int valor4;
+
+        System.out.print("Digite o primeiro valor: ");
+        valor1 = sc.nextInt();
+
+        System.out.print("Digite o segundo valor: ");
+        valor2 = sc.nextInt();
+
+        System.out.print("Digite o terceiro valor: ");
+        valor3 = sc.nextInt();
+
+        System.out.print("Digite o quarto valor: ");
+        valor4 = sc.nextInt();
+
+        int produto = valor1 * valor2 - valor3 * valor4;
+
+        System.out.printf("A produto dos valores é: " + produto);
+    }
 }
