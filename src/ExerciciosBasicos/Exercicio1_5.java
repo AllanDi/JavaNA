ae023934c7037259ef0c281790cb0ffc5f5bc927
Allan <allandrs@gmail.com>
2023-05-20 23:27:08 -0300

    exercicio 5 adicionado

---
+++ src/ExerciciosBasicos/Exercicio1_5.java
@@ -0,0 +1,18 @@
+package ExerciciosBasicos;
+
+import java.util.Scanner;
+
+public class Exercicio1_5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Exercicio 3");
+        System.out.println("Fazer um programa para ler o código de uma peça 1, o número de peças 1, " +
+                "o valor unitário de cada peça 1, o\n" +
+                "código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. " +
+                "Calcule e mostre o valor a ser pago.");
+
+
+    }
+}
