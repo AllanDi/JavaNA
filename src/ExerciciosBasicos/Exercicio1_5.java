35afb107a572b96641bbbcf231d6375fa6722d97
Allan <allandrs@gmail.com>
2023-05-21 00:59:21 -0300

    correção de valores

--- src/ExerciciosBasicos/Exercicio1_5.java
+++ src/ExerciciosBasicos/Exercicio1_5.java
@@ -7,7 +7,7 @@ public class Exercicio1_5 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Exercicio 3");
+        System.out.println("Exercicio 5");
         System.out.println("Fazer um programa para ler o código de uma peça 1, o número de peças 1, " +
                 "o valor unitário de cada peça 1, o\n" +
                 "código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. " +
