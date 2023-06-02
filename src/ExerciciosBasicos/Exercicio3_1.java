7dfac6abc6bb78506df0845705332d1b6986fa97
Allan <allandrs@gmail.com>
2023-06-02 14:57:16 -0300

    Continuação exercicios

--- src/ExerciciosBasicos/Exercicio3_1.java
+++ src/ExerciciosBasicos/Exercicio3_1.java
@@ -1,4 +1,23 @@
 package ExerciciosBasicos;
 
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Exercicio3_1 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Exercicio 1");
+        System.out.println("Escreva um programa que repita a leitura de uma senha até que ela seja válida. " +
+                "Para cada leitura de senha\n" +
+                "incorreta informada, escrever a mensagem \"Senha Invalida\". Quando a senha for informada " +
+                "corretamente deve ser\n" +
+                "impressa a mensagem \"Acesso Permitido\" e o algoritmo encerrado. Considere que a senha " +
+                "correta é o valor 2002.");
+
+
+    }
 }
