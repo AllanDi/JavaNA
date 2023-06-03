837b786eca420deee4659c836f3a9a4bac778418
Allan <allandrs@gmail.com>
2023-06-03 09:50:10 -0300

    Continuação exercicios

---
+++ src/ExerciciosBasicos/Exercicio3_2.java
@@ -0,0 +1,21 @@
+package ExerciciosBasicos;
+
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio3_2 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Exercicio 2");
+        System.out.println("Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de " +
+                "pontos no sistema\n" +
+                "cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado " +
+                "quando pelo\n" +
+                "menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).");
+
+    }
+}
