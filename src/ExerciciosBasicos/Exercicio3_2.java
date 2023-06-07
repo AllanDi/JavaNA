6f2230a18ebee4caba5c7ae45d81d76156102a10
Allan <allandrs@gmail.com>
2023-06-07 11:30:17 -0300

    Continuação exercicios

--- src/ExerciciosBasicos/Exercicio3_2.java
+++ src/ExerciciosBasicos/Exercicio3_2.java
@@ -13,7 +13,8 @@ public class Exercicio3_2 {
         System.out.println("Exercicio 2");
         System.out.println("Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de " +
                 "pontos no sistema\n" +
-                "cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado " +
+                "cartesiano. Para cada ponto escrever o quadrante a que ele pertence. " +
+                "O algoritmo será encerrado " +
                 "quando pelo\n" +
                 "menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).");
 
