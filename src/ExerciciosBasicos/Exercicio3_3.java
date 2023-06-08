501cd699a256533a4e3bbc9cfcb26e7d7484e7be
Allan <allandrs@gmail.com>
2023-06-08 12:36:26 -0300

    Continuacao exercicios

---
+++ src/ExerciciosBasicos/Exercicio3_3.java
@@ -0,0 +1,17 @@
+package ExerciciosBasicos;
+
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio3_3 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Exercicio 3");
+        System.out.println("");
+
+    }
+}
