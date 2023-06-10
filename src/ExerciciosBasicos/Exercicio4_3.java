a6554c26a5c245a84ae3684d20b21d3c9b62f480
Allan <allandrs@gmail.com>
2023-06-09 22:33:41 -0300

    Continuação exercicios

--- src/ExerciciosBasicos/Exercicio4_3.java
+++ src/ExerciciosBasicos/Exercicio4_3.java
@@ -12,6 +12,12 @@ public class Exercicio4_3 {
 
         System.out.println("Exercicio 3");
 
-        System.out.println("");
+        System.out.println("Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. " +
+                "Cada caso de teste consiste\n" +
+                "de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um " +
+                "destes\n" +
+                "conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o " +
+                "terceiro valor tem\n" +
+                "peso 5.");
     }
 }
