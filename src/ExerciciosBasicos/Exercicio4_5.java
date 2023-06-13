34542260c9e360b940fb15e3662fdc9449049789
Allan <allandrs@gmail.com>
2023-06-12 21:02:33 -0300

    Continuação exercicios

---
+++ src/ExerciciosBasicos/Exercicio4_5.java
@@ -0,0 +1,26 @@
+package ExerciciosBasicos;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio4_5 {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Exercicio 5");
+
+        System.out.println("Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de " +
+                "N = N * (N-1) * (N-2) * (N-3) * ... * 1.\n" +
+                "Lembrando que, por definição, fatorial de 0 é 1.");
+
+
+        System.out.println("Informe o numero para se calcular o fatorial:  ");
+        int numero = sc.nextInt();
+
+        for (int i = 0; i <= numero; i++) {
+
+
+        }
+    }
+}
---
+++ src/ExerciciosBasicos/Exercicio4_6.java
@@ -0,0 +1,15 @@
+package ExerciciosBasicos;
+
+public class Exercicio4_6 {
+    public static void main(String[] args) {
+
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The length of the txt string is: " + txt.length());
+
+        String firstName = "John ";
+        String lastName = "Doe";
+        System.out.println(firstName.concat(lastName));
+        System.out.println(firstName + lastName);
+
+    }
+}
