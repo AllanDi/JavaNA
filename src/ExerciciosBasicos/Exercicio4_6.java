8e5d0e58b653e7fd7efd0c6f67c398530c00cc81
Allan <allandrs@gmail.com>
2023-06-18 10:30:47 -0300

    continuacao exercicios

--- src/ExerciciosBasicos/Exercicio4_6.java
+++ src/ExerciciosBasicos/Exercicio4_6.java
@@ -12,6 +12,16 @@ public class Exercicio4_6 {
         System.out.println("Exercicio 6");
 
         System.out.println("Ler um número inteiro N e calcular todos os seus divisores.\n");
+
+        System.out.println("Digite um numero para ser calculado seus divisores: ");
+         int numero = sc.nextInt();
+
+         for(int i = 1; i <= numero; i++){
+
+             if(numero % i == 0){
+                 System.out.println(i);
+             }
+         }
     }
 }
 
