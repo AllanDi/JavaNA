9f6b4939d517caf8930fc0f5ca22db42dab7c906
Allan <allandrs@gmail.com>
2023-06-20 14:07:17 -0300

    Continuação exercicios

--- src/ExerciciosBasicos/Exercicio4_8.java
+++ src/ExerciciosBasicos/Exercicio4_8.java
@@ -22,7 +22,7 @@ public class Exercicio4_8 {
             double C = sc.nextDouble();
             double F = 9.0 * C / 5.0 + 32.0;
             System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
-            System.out.print("Deseja repetir (s/n)? ");
+            System.out.print("Deseja repetir (s/n)?");
             resp = sc.next().charAt(0);
         } while (resp != 'n');
 
