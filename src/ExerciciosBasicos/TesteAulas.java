296f0f9d3ffa642c796981912d768dc0761ad9d8
Allan <allandrs@gmail.com>
2023-07-03 12:26:05 -0300

    Continuação exercicios

---
+++ src/ExerciciosBasicos/TesteAulas.java
@@ -0,0 +1,16 @@
+package ExerciciosBasicos;
+
+import java.util.Scanner;
+
+public class TesteAulas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite três numeros: ");
+        int numero1 = sc.nextInt();
+        int numero2 = sc.nextInt();
+        int numero03 = sc.nextInt();
+
+
+        }
+    }
+}
