70efcef198ce5db637a3e5fd9c8c5a128c054fb5
Allan <allandrs@gmail.com>
2023-08-07 21:10:26 -0300

    Continuação exercicios

--- src/exerciciosBasicos/Exercicio1_4.java
+++ src/exerciciosBasicos/Exercicio1_4.java
@@ -10,7 +10,7 @@ public class Exercicio1_4 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Exercicio 3");
-        System.out.println("Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, " +
+        System.out.println("Fazer umprograma que leia o número de um funcionário, seu número de horas trabalhadas, " +
                 "o valor que recebe por hora e calcula o salário desse funcionário. A seguir, " +
                 "mostre o número e o salário do funcionário, com duas casas\n" +
                 "decimais.");
